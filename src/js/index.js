4a701455eabf554b078d28035724f862000ad6f5
JavyProgrameitor <85842248+JavyProgrameitor@users.noreply.github.com>
2024-09-25 13:50:54 +0200

    The portfolio  with access to two applications

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let cards = document.querySelectorAll('.card');
+  
+    cards.forEach(card => {
+      card.addEventListener('mouseover', (e) => {
+        let positionX = window.innerWidth;
+        let positionY = window.innerHeight;
+  
+        card.style.transform = `rotateY(${(e.clientX / positionX - 0.75) * 50}deg) rotateX(${(e.clientY / positionY - 0.75) * 50}deg) scale(1.2)`;
+      });
+  
+      card.addEventListener('mouseout', () => {
+        card.style.transform = ''; 
+      });
+    });
+  });
+  
